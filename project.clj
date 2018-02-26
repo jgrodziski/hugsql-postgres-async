8ddf8db87de60afcc1a1ba3b26f45b341b2598e3
jeremiegrodziski <jeremie.github@grodziski.com>
2018-02-26 22:51:51 +0100

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject skinney/hugsql-adapter-postgres-async "0.3.3"
+(defproject jgrodziski/hugsql-adapter-postgres-async "0.3.4"
   :description "Postgres.async adapter for HugSQL"
-  :url "https://www.github.com/Skinney/hugsql-postgres-async"
+  :url "https://www.github.com/jgrodziski/hugsql-postgres-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/core.async "0.2.374" :scope "provided"]
-                 [com.layerware/hugsql-adapter "0.3.1"]
-                 [alaisi/postgres.async "0.6.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/core.async "0.3.465" :scope "provided"]
+                 [com.layerware/hugsql-adapter "0.4.8"]
+                 [alaisi/postgres.async "0.8.0"]])
