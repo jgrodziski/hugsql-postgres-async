cb49001fb8edfb65320c12840ecb7b4891ee8626
jeremiegrodziski <jeremie.github@grodziski.com>
2018-03-06 19:48:31 +0000

    use a fork of postgresql-async driver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jgrodziski/hugsql-adapter-postgres-async "0.3.4"
+(defproject jgrodziski/hugsql-adapter-postgres-async "0.4.0"
   :description "Postgres.async adapter for HugSQL"
   :url "https://www.github.com/jgrodziski/hugsql-postgres-async"
   :license {:name "Apache License, Version 2.0"
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/core.async "0.3.465" :scope "provided"]
                  [com.layerware/hugsql-adapter "0.4.8"]
-                 [alaisi/postgres.async "0.8.0"]])
+                 [com.github.jaceksokol/postgres-async-driver "0.1.3"]])
